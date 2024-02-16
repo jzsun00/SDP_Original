2ac7c7fd06c5c6db8d80c9321b2d4ce12cd9f5c8
Jiazheng Sun <js1106@duke.edu>
2024-02-16 17:01:26 -0500

    Added Monomial

--- New_Code/operators.hpp
+++ New_Code/operators.hpp
@@ -10,6 +10,10 @@
 #include <cstdlib>
 #include <iostream>
 #include <limits>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <string>
 
 class LadderOp {
  protected:
@@ -17,27 +21,96 @@ class LadderOp {
   bool creatorF;
 
  public:
-  /*Constructs a ladder operator with specified index and creatorF,
+  /*Construct a ladder operator with specified index and creatorF,
    default constructor use INT_MIN and false.*/
   LadderOp() : index(std::numeric_limits<int>::min()), creatorF(false) {}
   LadderOp(int i_idx, bool i_creatorF) : index(i_idx), creatorF(i_creatorF) {}
+  LadderOp(LadderOp const & rhs) {
+    index = rhs.index;
+    creatorF = rhs.creatorF;
+  }
+  ~LadderOp() {}
   /*Get information of the ladder operator.*/
   int getIndex() const { return index; }
   bool getCreatorF() const { return creatorF; }
-  /*Overload operators.*/
-  bool operator==(LadderOp const & v) const {
-    return creatorF == v.creatorF && index == v.index;
+  std::string toString() const {
+    std::string ans = "a_";
+    ans += std::to_string(index);
+    if (creatorF) {
+      ans += "+";
+    }
+    return ans;
   }
-  bool operator<(LadderOp const & v) const {
-    if (creatorF != v.creatorF) {
-      return creatorF < v.creatorF;
+  /*Overload operators.*/
+  LadderOp & operator=(LadderOp const & rhs) {
+    index = rhs.index;
+    creatorF = rhs.creatorF;
+    return *this;
+  }
+  bool operator==(LadderOp const & rhs) const {
+    return creatorF == rhs.creatorF && index == rhs.index;
+  }
+  bool operator<(LadderOp const & rhs) const {
+    if (creatorF != rhs.creatorF) {
+      throw std::invalid_argument("Two operators are not the same kind!\n");
     }
     else {
-      return index < v.index;
+      return index < rhs.index;
     }
   }
-  bool operator>(LadderOp const & v) const { return !(*this < v || *this == v); }
+  bool operator>(LadderOp const & rhs) const { return !(*this < rhs || *this == rhs); }
   void herm() { creatorF ^= 1; }
+};
+
+class Monomial {
+ protected:
+  std::vector<LadderOp> Expr;
+  
+ public:
+  /*Construct a monomial with one ladder operator or copy another,
+   default constructor use an empty vector.*/
+  Monomial(): Expr() {}
+  Monomial(LadderOp & Op): Expr(1, Op) {}
+  Monomial(Monomial const & rhs) {
+    Expr = rhs.Expr;
+  }
+  ~Monomial() {}
+  /*Get information of the monomial.*/
+  size_t getSize() const {
+    return Expr.size();
+  }
+  std::string toString() {
+    std::string ans = "";
+    for (std::vector<LadderOp>::iterator it = Expr.begin(); it != Expr.end(); ++it) {
+      ans += it->toString();
+    }
+    return ans;
+  }
+  /*Overload operators.*/
+  Monomial & operator=(Monomial const & rhs) {
+    Expr = rhs.Expr;
+    return *this;
+  }
+  bool operator==(Monomial const & rhs) const {
+    return Expr == rhs.Expr;
+  }
+  LadderOp operator[](size_t n) const {
+    return Expr[n];
+  }
+  Monomial & operator*=(Monomial const & rhs) {
+    Expr.insert(Expr.end(), rhs.Expr.begin(), rhs.Expr.end());
+    return *this;
+  }
+  Monomial & operator*=(LadderOp const & toAdd) {
+    Expr.push_back(toAdd);
+    return *this;
+  }
+  void herm() {
+    std::reverse(Expr.begin(), Expr.end());
+    for (std::vector<LadderOp>::iterator it = Expr.begin(); it != Expr.end(); ++it) {
+      it->herm();
+    }
+  }
 };
 
 #endif
--- New_Code/operators_test.cpp
+++ New_Code/operators_test.cpp
@@ -2,13 +2,51 @@
 
 #include <cassert>
 
-int main(void) {
+int LadderOpTest(void) {
   LadderOp * op1 = new LadderOp(0, true);
   LadderOp * op2 = new LadderOp(1, true);
-  LadderOp * op3 = new LadderOp(2, false);
-  assert(op1->getIndex() == 0);
-  assert(op1->getCreatorF() == true);
+  LadderOp * op3 = new LadderOp();
+  LadderOp * op4 = new LadderOp(1, true);
+  LadderOp * op5 = new LadderOp(*op1);
+  LadderOp * op6 = new LadderOp();
+  *op6 = *op1;
+  assert(op2->getIndex() == 1);
+  assert(op2->getCreatorF() == true);
+  op3->herm();
+  assert(op3->getIndex() == std::numeric_limits<int>::min());
+  assert(op3->getCreatorF() == true);
+  assert(*op5 == *op1);
+  assert(op5 != op1);
+  std::cout << "Constructor tests pass!" << std::endl;
+  assert((*op2 == *op4) == true);
+  assert((*op2 > *op1) == true);
   assert((*op1 < *op2) == true);
-  std::cout << "The program runs succeccfully.";
+  assert((*op1 == *op2) == false);
+  assert(*op6 == *op1);
+  assert(op6 != op1);
+  try {
+    op3->herm();
+    *op2 > *op3;
+  }
+  catch (std::invalid_argument & e) {
+    std::cerr << "Caught exception: " << e.what() << std::endl;
+  }
+  std::cout << "Operator overload tests pass!" << std::endl;
+  op2->herm();
+  std::cout << op1->toString() << std::endl;
+  std::cout << op2->toString() << std::endl;
+  std::cout << "toString method tests pass!" << std::endl;
+  delete op1;
+  delete op2;
+  delete op3;
+  delete op4;
+  delete op5;
+  delete op6;
+  std::cout << "The program runs succeccfully!" << std::endl;
+  return EXIT_SUCCESS;
+}
+
+int main() {
+  assert(LadderOpTest() == EXIT_SUCCESS);
   return EXIT_SUCCESS;
 }
