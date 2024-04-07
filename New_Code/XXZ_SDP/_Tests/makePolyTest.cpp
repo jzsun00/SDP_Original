8a9a760a78eff431d6575f0557afd2feb70713b7
Jiazheng Sun <js1106@duke.edu>
2024-04-07 11:37:47 -0400

    Now can produce the vector

--- New_Code/XXZ_SDP/_Tests/makePolyTest.cpp
+++ New_Code/XXZ_SDP/_Tests/makePolyTest.cpp
@@ -1,10 +1,11 @@
+#include "../../HardCore/hardCoreSubspaces.hpp"
 #include "../hamiltonians_XXZ.hpp"
 
 using std::cout;
 using std::endl;
 
 int main(void) {
-  size_t sites = 4;
+  size_t sites = 3;
   double Jz = 0.2;
   HardCorePolynomial<HardCoreMonomial<HardCore1DLadderOp> > poly1 = makePoly(sites, Jz);
   cout << "sites = " << sites << "\nJz = " << Jz << endl;
@@ -12,6 +13,21 @@ int main(void) {
   poly1.normalize();
   cout << "After normalization\n"
        << "poly1 = " << poly1.toString() << endl;
+  cout << "\nNow construct the spaces" << endl;
+  HardCore1DOpSubBasis sub2(0, 3, 2);
+  HardCore1DOpSubBasis sub4(0, 3, 4);
+  sub2.init();
+  sub4.init();
+  cout << sub2.toString() << endl;
+  cout << sub4.toString() << endl;
+  HardCore1DOpBasis basis;
+  basis.addSubspace(sub2);
+  basis.addSubspace(sub4);
+  cout << "poly1 = " << poly1.toString() << endl;
+  cout << "\nCombine two subspaces:" << endl;
+  cout << basis.toString() << endl;
+  vector<complex<double> > proj1 = basis.projPoly(poly1);
+  cout << complexVector_toString(proj1) << endl;
   cout << "Tests pass!" << endl;
   return EXIT_SUCCESS;
 }
